8eb24218245d7492b0e3fa9c0b9f19653282aee4
gaelgoth <gaelgoth@users.noreply.github.com>
2024-08-17 16:16:24 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="gaelgoth"
+original_name="snapmetrics"
+original_urlname="snapmetrics"
+original_description="Awesome snapmetrics created by gaelgoth"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv snapmetrics $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
